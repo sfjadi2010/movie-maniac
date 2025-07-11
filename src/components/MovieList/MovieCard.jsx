39d1252a952a43a8e895c834c78bcc8c1221b63c
Salman Farsi <sfjadi2010@live.com>
2025-07-11 17:11:44 -0400

    feat: implement movie fetching and display in MovieList component

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -2,32 +2,31 @@ import React from "react";
 import "./MovieCard.css";
 
 import Star from "../../assets/glowing-star.png";
-const MovieCard = () => {
+
+const MovieCard = ({ movie }) => {
   return (
-    <>
-      <a href="" className="movie_card">
-        <img
-          src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg"
-          alt="Movie Poster"
-          className="movie_poster"
-        />
+    <a href={`https://www.themoviedb.org/movie/${movie.id}`} className="movie_card" target="_blank" rel="noopener noreferrer">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt="Movie Poster"
+        className="movie_poster"
+      />
 
-        <div className="movie_details">
-          <h3 className="movie_details_heading">Movie Name</h3>
-          <div className="align_center movie_date_rate">
-            <p>10-20-2020</p>
-            <p>
-              8.0 <img src={Star} alt="rating icon" className="card_emoji" />
-            </p>
-          </div>
-
-          <p className="movie_description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      <div className="movie_details">
+        <h3 className="movie_details_heading">{movie.original_title}</h3>
+        <div className="align_center movie_date_rate">
+          <p>{movie.release_date}</p>
+          <p>
+            {movie.rate_average}{" "}
+            <img src={Star} alt="rating icon" className="card_emoji" />
           </p>
         </div>
-      </a>
-    </>
+
+        <p className="movie_description">
+          {movie.overview.slice(0, 100) + "..."}
+        </p>
+      </div>
+    </a>
   );
 };
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,41 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 import Fire from "../../assets/fire.png";
 import Start from "../../assets/glowing-star.png";
 import Party from "../../assets/partying-face.png";
-import MovieCard from './MovieCard';
+import MovieCard from "./MovieCard";
 
 const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    // Fetch movies data here
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=3a0b2acf5787dec8795880fdae9ed3fa&language=en-US&page=${currentPage}`
+    );
+    const data = await response.json();
+
+    setMovies(data.results);
+  };
+
   return (
     <section className="movie_list">
-        <header className="align_center movie_list_header">
-            <h2 className="align_center movie_list_heading">Popular <img src={Fire} alt="Popular" className="navbar_emoji" /></h2>
+      <header className="align_center movie_list_header">
+        <h2 className="align_center movie_list_heading">
+          Popular <img src={Fire} alt="Popular" className="navbar_emoji" />
+        </h2>
 
-            <div className="align_center">
-                <ul className="align_center movie_filter">
-                    <li className="movie_filter_item active">8+ Star</li>
-                    <li className="movie_filter_item">7+ Start</li>
-                    <li className="movie_filter_item">6+ Star</li>
-                </ul>
+        <div className="align_center">
+          <ul className="align_center movie_filter">
+            <li className="movie_filter_item active">8+ Star</li>
+            <li className="movie_filter_item">7+ Start</li>
+            <li className="movie_filter_item">6+ Star</li>
+          </ul>
 
-                <select name="" id="" className="movie_sorting">
-                    <option value="">SortBy</option>
-                    <option value="">Date</option>
-                    <option value="">Rating</option>
-                </select>
+          <select name="" id="" className="movie_sorting">
+            <option value="">SortBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
+          </select>
 
-                <select name="" id="" className="movie_sorting">
-                    <option value="">Ascending</option>
-                    <option value="">Descending</option>
-                </select>
-            </div>
-        </header>
-
-        <div className="align_center movie_cards">
-            <MovieCard />
+          <select name="" id="" className="movie_sorting">
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
+          </select>
         </div>
-    </section>
-  )
-}
+      </header>
 
-export default MovieList
+      <div className="align_center movie_cards">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default MovieList;
